c8d82730588cf020c3198fd3a581b7657e310927
carlCarlson6 <bullet.carlos@gmail.com>
2020-02-21 20:46:53 +0100

    added more initial files

---
+++ NERwithBERT.Models/Document.py
@@ -0,0 +1,30 @@
+
+class Document:
+    self.DocId = str
+    self.Text = str
+    self.Labels = str
+    self.TokenizedText = None
+    self.TokenizedLabels = None
+    self.Inputs = None 
+    self.Tags = None
+    self.Masks = None
+    self.TokenizedPredictedTags = None
+    self.Predictions = None
+    self.Entities = []
+
+    def __init__(self, docId=None, text=None, labels=None, tokenizedText=None, tokenizedLabels=None, inputs=None, tags=None, masks=None):
+        self.DocId = docId
+        self.Text = text
+        self.Labels = labels
+
+        self.TokenizedText = tokenizedText
+        self.TokenizedLabels = tokenizedLabels
+
+        self.Inputs = inputs 
+        self.Tags = tags
+        self.Masks = masks
+
+        self.TokenizedPredictedTags = None
+
+        self.Predictions = None
+        self.Entities = []
